4f2554abdcc20d19c1dc6c839f4da0e08f8daa20
khakaa <phr0210@naver.com>
2023-08-29 02:31:05 +0900

    Feat : 임시 kanban 라우트 추가 #3

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = React.lazy(() => import('./pages/home/Home'));
+const Kanban = React.lazy(() => import('./pages/kanban/Kanban'));
 
-const routes = [{ path: '/', element: Home }];
+const routes = [
+  { path: '/', element: Home },
+  { path: '/kanban', element: Kanban },
+];
 
 export default routes;
