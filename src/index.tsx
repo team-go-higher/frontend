3db4780dc9f327851e3f246c97cc99b3ceaef93b
khakaa <phr0210@naver.com>
2023-09-21 01:01:09 +0900

    Feat : QueryClient 주입

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { GlobalStyles } from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 import store from './redux/store';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
