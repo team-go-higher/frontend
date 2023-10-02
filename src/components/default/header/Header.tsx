3ecb457375d7a4cd0bdb0a55cf9d965e96dfaec4
전수빈 <soobin08011812@gmail.com>
2023-10-02 21:30:31 +0900

    Fix: 헤더 디자인 수정 반영 및 메뉴바 너비 고정 #33

--- src/components/default/header/Header.tsx
+++ src/components/default/header/Header.tsx
@@ -7,7 +7,7 @@ import {
 import AlarmImg from 'assets/header/header_alarm.svg';
 import ArrowDownImg from 'assets/header/header_arrow_down.svg';
 
-const MenuItemArr = ['내 공고 리스트', '공고리스트', '지원서 추가', '마이페이지'];
+const MenuItemArr = ['내 공고 리스트', '공고리스트', '지원서 추가'];
 
 const Header = () => {
   const [isSelect, setIsSelect] = useState('공고리스트');
@@ -16,26 +16,28 @@ const Header = () => {
     <HeaderContainer>
       <div className='headerContainer'>
         <div className='headerLogo'>Go-Higher</div>
-        <HeaderMenuContainer>
-          {MenuItemArr.map((e, index) => {
-            return (
-              <div
-                className={`menuItem ${isSelect === e ? 'active' : ''}`}
-                key={index}
-                onClick={() => setIsSelect(e)}>
-                {e}
-              </div>
-            );
-          })}
-        </HeaderMenuContainer>
-        <HeaderPersonalContainer>
-          <img src={AlarmImg} className='alarmImg' alt='alarmImg' />
-          <div className='personalBox'>
-            <div className='profile'>고하</div>
-            <div className='profileName'>사용자</div>
-            <img className='arrowDown' alt='arrowDownImg' src={ArrowDownImg} />
-          </div>
-        </HeaderPersonalContainer>
+        <div className='rightContainer'>
+          <HeaderMenuContainer>
+            {MenuItemArr.map((e, index) => {
+              return (
+                <div
+                  className={`menuItem ${isSelect === e ? 'active' : ''}`}
+                  key={index}
+                  onClick={() => setIsSelect(e)}>
+                  {e}
+                </div>
+              );
+            })}
+          </HeaderMenuContainer>
+          <HeaderPersonalContainer>
+            <img src={AlarmImg} className='alarmImg' alt='alarmImg' />
+            <div className='personalBox'>
+              <div className='profile'>고하</div>
+              <div className='profileName'>사용자</div>
+              <img className='arrowDown' alt='arrowDownImg' src={ArrowDownImg} />
+            </div>
+          </HeaderPersonalContainer>
+        </div>
       </div>
     </HeaderContainer>
   );
