34f7ffeef29e80236eaa87d9999e04700d79f1fe
전수빈 <soobin08011812@gmail.com>
2023-10-02 18:32:36 +0900

    Feat: 회원가입 - 희망직무 선택 페이지 퍼블리싱 #32

---
+++ src/pages/signUp/DesiredPosition.tsx
@@ -0,0 +1,125 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+
+const PositionList = [
+  '디자인',
+  '법률',
+  '금융',
+  'CS',
+  '마케팅',
+  '개발',
+  '작가',
+  '영업',
+  '교육',
+  '회계',
+  '세무',
+];
+
+const DesiredPosition = () => {
+  const [position, setPosition] = useState<string[]>([]);
+
+  const handlePosition = (item: string) => {
+    if (position.includes(item)) {
+      const newPositionArr = position.filter(number => {
+        return number !== item;
+      });
+      setPosition(newPositionArr);
+    } else {
+      setPosition([...position, item]);
+    }
+  };
+
+  return (
+    <Main>
+      <Wrapper>
+        <div className='mainTitle'>희망 직무를 선택하세요</div>
+        <PositionContainer>
+          {PositionList.map((item, index) => {
+            return (
+              <PositionCardContainer
+                active={position.includes(item)}
+                key={index}
+                onClick={() => {
+                  handlePosition(item);
+                }}>
+                {item}
+              </PositionCardContainer>
+            );
+          })}
+          <PositionCardContainer active={false}>기타(직접입력)</PositionCardContainer>
+        </PositionContainer>
+        <div className='requestPosition'>
+          나의 직무가 없나요? <span>요청하기</span>
+        </div>
+        <DefaultBtn active={position.length > 0}>고하이어 시작하기</DefaultBtn>
+      </Wrapper>
+    </Main>
+  );
+};
+
+export default DesiredPosition;
+
+const Main = styled.div`
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 135px;
+  .mainTitle {
+    color: #3253ff;
+    text-align: center;
+    font-size: 30px;
+    font-weight: 700;
+    letter-spacing: -0.6px;
+  }
+  .requestPosition {
+    margin-bottom: 38px;
+    color: #969696;
+    text-align: center;
+    font-size: 17px;
+    font-weight: 500;
+    line-height: 24px;
+    span {
+      color: #3253ff;
+      cursor: pointer;
+    }
+  }
+`;
+
+const PositionContainer = styled.div`
+  display: grid;
+  margin: 38px 0 23px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 56px 1fr;
+  gap: 12px;
+`;
+
+const PositionCardContainer = styled.div<{ active: boolean }>`
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  border: 0.5px solid #d9d9d9;
+  background: ${props => (props.active ? '#333' : '#fff')};
+  width: 239px;
+  height: 56px;
+  color: ${props => (props.active ? '#fff' : '#333')};
+  text-align: center;
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const DefaultBtn = styled(PositionCardContainer)<{ active: boolean }>`
+  color: ${props => (props.active ? '#fff' : '#d9d9d9')};
+  width: 296px;
+  cursor: ${props => (props.active ? 'pointer' : 'default')};
+  background: ${props => (props.active ? '#3253FF' : '#fff')};
+`;
