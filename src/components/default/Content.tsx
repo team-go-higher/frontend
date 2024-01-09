79356401f4a9bf693a3f2d868adfefd3b6de0ac1
khakaa <phr0210@naver.com>
2024-01-09 10:29:14 +0900

    fix : private route, public route 분리 완료 #39

--- src/components/default/Content.tsx
+++ src/components/default/Content.tsx
@@ -1,11 +1,27 @@
 import React, { Suspense } from 'react';
-import { useRoutes } from 'react-router-dom';
-import routes from 'routes';
+import { Route, Routes } from 'react-router-dom';
+import { privateRoutes, publicRoutes } from 'routes';
+import PrivateRoutesComponent from './PrivateRoutesComponent';
 
 const loading = <div>화면을 불러오는 중 입니다.</div>;
 
 const AppContent = () => {
-  return <Suspense fallback={loading}>{useRoutes(routes)}</Suspense>;
+  return (
+    <Suspense fallback={loading}>
+      <Routes>
+        {/* private route */}
+        <Route path='/' element={<PrivateRoutesComponent />}>
+          {privateRoutes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
+        {/* public route */}
+        {publicRoutes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default AppContent;
