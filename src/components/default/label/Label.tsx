4bb29660cf9ee287dcf714fd98450cbc77172a12
jinsil <jinsil378900@gmail.com>
2024-02-14 01:28:59 +0900

    Fix: div에서 label로 수정

--- src/components/default/label/Label.tsx
+++ src/components/default/label/Label.tsx
@@ -13,18 +13,20 @@ export const TYPE_PROCESS = {
   COMPLETE: 'rgb(var(--defaultRed))',
 };
 
-const StyledLabel = styled.div<LabelProps>`
-  display: inline-block;
-  padding: 4px 10px;
-  margin-bottom: 10px;
+const StyledLabel = styled.label<LabelProps>`
+  width: fit-content;
+  height: 22px;
+  white-space: nowrap;
+  padding: 0 10px;
   outline: none;
   border: 1px solid ${props => props.process && TYPE_PROCESS[props.process]};
+  border-radius: 12.5px;
   background-color: ${props =>
     props.isPast ? props.process && TYPE_PROCESS[props.process] : 'rgb(var(--white));'};
   color: ${props =>
     props.isPast ? 'rgb(var(--white));' : props.process && TYPE_PROCESS[props.process]};
-  border-radius: 12.5px;
   font-size: 14px;
+  cursor: default;
 `;
 
 export const Label = ({ process, isPast = true }: LabelProps) => {
